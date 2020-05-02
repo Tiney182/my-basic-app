029919217934ee3acde79bf9e8966f71565b6160
Andrew Hickman <axh@phoebussoftware.com>
2020-05-02 18:40:42 +0100

    Adding a for loop array of server

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() { 
     setTimeout(() => {
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
 
   onCreateSever() {
     this.serverCreationStatus  ='Server was created!';
+    this.servers.push(this.serverName);
     this.serverCreated = true;
   }
 
